c68b70af66ae1a32c82eb60396806b49914e17f5
downloador <38085070+downloador@users.noreply.github.com>
2021-04-08 20:40:39 +0200

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -8,14 +8,14 @@ module.exports = {
       var number = vars.msg.content.split(" ")[1]
       if (isNaN(number) === false) {
         if (number > 100 || number < 2) {
-          msg.reply("Nummret kan inte vara större eller mindre än 100 och 2")
+          vars.msg.reply("Nummret kan inte vara större eller mindre än 100 och 2")
           return;
         }
         vars.msg.channel.bulkDelete(number).then( () => {
           vars.channel.send("Tog bort " + number + " meddelanden")
         })
       } else {
-       msg.reply("Du måste ange ett nummer") 
+       vars.msg.reply("Du måste ange ett nummer") 
       }
     }
   }
