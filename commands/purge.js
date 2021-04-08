568b6649ee65d4af529faca3cb18f9c46ba856fa
phpMySven <82169925+phpMySven@users.noreply.github.com>
2021-04-08 23:22:57 +0200

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -8,7 +8,7 @@ module.exports = {
       var number = vars.msg.content.split(" ")[1]
       if (isNaN(number) === false) {
         if (number > 100 || number < 2) {
-          vars.msg.reply("Nummret kan inte vara större eller mindre än 100 och 2")
+          vars.msg.reply("Nummret kan inte vara större än 100 eller mindre än 2.")
           return;
         }
         vars.msg.channel.bulkDelete(number).then( () => {
