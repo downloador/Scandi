870f90da6fdc546af37a2947d081c2401be51e11
downloador <38085070+downloador@users.noreply.github.com>
2021-04-08 18:14:31 +0200

    indent

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,11 @@
 module.exports = {
-	name: 'ping',
-    cooldown: 1,
-    level: 0,
-	execute(vars) {
-        var result = vars.canExecuteCommand(this,vars)
-        if (result === true) {
-            vars.msg.reply(":ping_pong: Svarstiden på botten är " + vars.client.ws.ping + " millisekunder sakta.")
-        }
-	},
+  name: 'ping',
+  cooldown: 1,
+  level: 0,
+  execute(vars) {
+    var result = vars.canExecuteCommand(this, vars)
+    if (result === true) {
+      vars.msg.reply(":ping_pong: Svarstiden på botten är " + vars.client.ws.ping + " millisekunder sakta.")
+    }
+  }
 };
