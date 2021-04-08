4888009a7a481a1f26da1b90cafcff32ebcb4c42
downloador <38085070+downloador@users.noreply.github.com>
2021-04-08 20:32:53 +0200

    Create purge.js

---
+++ commands/purge.js
@@ -0,0 +1,22 @@
+module.exports = {
+  name: 'ping',
+  cooldown: 3,
+  level: 3,
+  execute(vars) {
+    var result = vars.canExecuteCommand(this, vars)
+    if (result === true) {
+      var number = vars.msg.split(" ")[1]
+      if (isNaN(number) === false) {
+        if (number > 100 || number < 2) {
+          msg.reply("Nummret kan inte vara större eller mindre än 100 och 2")
+          return;
+        }
+        vars.msg.channel.bulkDelete(number).then( () => {
+          vars.channel.send("Tog bort " + number " meddelanden")
+        })
+      } else {
+       msg.reply("Du måste ange ett numer) 
+      }
+    }
+  }
+};
