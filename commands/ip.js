85df58335c23183e38595f323573133e79ccdc91
downloador <38085070+downloador@users.noreply.github.com>
2021-04-08 18:15:02 +0200

    indent

--- commands/ip.js
+++ commands/ip.js
@@ -1,11 +1,11 @@
 module.exports = {
-	name: 'ip',
-    cooldown: 5,
-    level: 0,
-	execute(vars) {
-        var result = vars.canExecuteCommand(this,vars)
-        if (result === true) {
-            vars.msg.reply("Direct Connect IP:n är **connect 83.233.127.52:30140**.")
-        }
-	},
+  name: 'ip',
+  cooldown: 5,
+  level: 0,
+  execute(vars) {
+    var result = vars.canExecuteCommand(this, vars)
+    if (result === true) {
+      vars.msg.reply("Direct Connect IP:n är **connect 83.233.127.52:30140**.")
+    }
+  }
 };
