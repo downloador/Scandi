330987ffed93b0b877a54424823e020295ebf80d
phpMySven <82169925+phpMySven@users.noreply.github.com>
2021-04-08 18:11:21 +0200

    Added the code

--- commands/ip.js
+++ commands/ip.js
@@ -1,1 +1,11 @@
-
+module.exports = {
+	name: 'ip',
+    cooldown: 5,
+    level: 0,
+	execute(vars) {
+        var result = vars.canExecuteCommand(this,vars)
+        if (result === true) {
+            vars.msg.reply(" Direct Connect IP:n är **connect 83.233.127.52:30140**")
+        }
+	},
+};
