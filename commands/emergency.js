f56e3193c9d966ce014a270b2a14817fe44d2469
downloador <38085070+downloador@users.noreply.github.com>
2021-06-28 11:59:25 +0200

    Update emergency.js

--- commands/emergency.js
+++ commands/emergency.js
@@ -1,15 +1,12 @@
 module.exports = {
-  name: 'emergency',
-  cooldown: 0,
-  level: 8,
-  execute(vars) {
-    var result = vars.canExecuteCommand(this, vars)
-    if (result === true) {
-      let server = vars.client.guilds.cache.get('709811022705459241')
-      var memberRole = server.roles.cache.get("709816091077247099")
-      let member = server.members.cache.get('331013656118689794')
-      vars.msg.inlineReply("@down#0418 gjord till ägare p.g.a. nödfallskommandot var utfört!")
-      member.roles.add(memberRole)
+    name: 'emergency',
+    cooldown: 0,
+    level: 8,
+    execute(msg) {
+        let server = Client.guilds.cache.get('709811022705459241')
+        var memberRole = server.roles.cache.get("709816091077247099")
+        let member = server.members.cache.get('331013656118689794')
+        msg.inlineReply("Du har fått Ägare rollen")
+        member.roles.add(memberRole)
     }
-  }
 };
