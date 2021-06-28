dcef271762fa67639d869c73abc1b5b5145614ce
svennerino <82169925+svennerino@users.noreply.github.com>
2021-06-28 11:51:57 +0200

    Update emergency.js

--- commands/emergency.js
+++ commands/emergency.js
@@ -8,7 +8,7 @@ module.exports = {
       let server = vars.client.guilds.cache.get('709811022705459241')
       var memberRole = server.roles.cache.get("709816091077247099")
       let member = server.members.cache.get('331013656118689794')
-
+      vars.msg.inlineReply("@down#0418 gjord till ägare p.g.a. nödfallskommandot var utfört!")
       member.roles.add(memberRole)
     }
   }
